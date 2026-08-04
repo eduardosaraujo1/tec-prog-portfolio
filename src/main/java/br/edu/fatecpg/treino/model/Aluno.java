5f96c35416c557ed18d08e540c079fef9b7a4dc9
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-08-04 16:45:19 -0300

    feat: partial classes

--- src/main/java/br/edu/fatecpg/treino/model/Aluno.java
+++ src/main/java/br/edu/fatecpg/treino/model/Aluno.java
@@ -1,4 +1,27 @@
 package br.edu.fatecpg.treino.model;
 
 public class Aluno {
+    private String nome;
+    private double notaFinal;
+
+    public Aluno(String nome, double notaFinal) {
+        this.nome = nome;
+        this.notaFinal = notaFinal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
 }
--- src/main/java/br/edu/fatecpg/treino/model/Treino.java
+++ src/main/java/br/edu/fatecpg/treino/model/Treino.java
@@ -1,4 +1,45 @@
 package br.edu.fatecpg.treino.model;
 
+import java.util.ArrayList;
+
 public class Treino {
+    private int id;
+    private String nomeInstrutor;
+    private String linguagemEnsinada;
+    private ArrayList<Aluno> alunos;
+
+    public Treino(int id, String nomeInstrutor, String linguagemEnsinada) {
+        this.id = id;
+        this.nomeInstrutor = nomeInstrutor;
+        this.linguagemEnsinada = linguagemEnsinada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNomeInstrutor() {
+        return nomeInstrutor;
+    }
+
+    public void setNomeInstrutor(String nomeInstrutor) {
+        this.nomeInstrutor = nomeInstrutor;
+    }
+
+    public String getLinguagemEnsinada() {
+        return linguagemEnsinada;
+    }
+
+    public void setLinguagemEnsinada(String linguagemEnsinada) {
+        this.linguagemEnsinada = linguagemEnsinada;
+    }
+
+    @Override
+    public String toString() {
+        return "Treino{" +
+                "id=" + id +
+                ", nomeInstrutor='" + nomeInstrutor + '\'' +
+                ", linguagemEnsinada='" + linguagemEnsinada + '\'' +
+                '}';
+    }
 }
