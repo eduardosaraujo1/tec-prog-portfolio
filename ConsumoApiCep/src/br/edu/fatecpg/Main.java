186060de1c0c5bbd6e549b8e9064392191a8f11e
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-08-18 17:50:50 -0300

    feat: historico concluido

--- ConsumoApiCep/src/br/edu/fatecpg/Main.java
+++ ConsumoApiCep/src/br/edu/fatecpg/Main.java
@@ -1,12 +1,21 @@
 package br.edu.fatecpg;
 
+import br.edu.fatecpg.model.Endereco;
+import br.edu.fatecpg.model.HistoricoEndereco;
 import br.edu.fatecpg.service.ApiEndereco;
 
 public class Main {
     public static void main(String[] args) {
         var api = new ApiEndereco();
+        var hist = new HistoricoEndereco();
+
         try {
-            System.out.print(api.obterEndereco("11702020"));
+            Endereco e1 = api.obterEndereco("11702020");
+            Endereco e2 = api.obterEndereco("11746164");
+
+            hist.addEndereco(e1);
+            hist.addEndereco(e2);
+            hist.exibirHistorico();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
---
+++ ConsumoApiCep/src/br/edu/fatecpg/model/HistoricoEndereco.java
@@ -0,0 +1,26 @@
+package br.edu.fatecpg.model;
+
+import java.util.ArrayList;
+
+public class HistoricoEndereco {
+    private ArrayList<Endereco> historico;
+
+    public HistoricoEndereco() {
+        historico = new ArrayList<>();
+    }
+
+    public void addEndereco(Endereco e) {
+        historico.add(e);
+    }
+
+    public void exibirHistorico() {
+        for (Endereco e : historico) {
+            System.out.println(e);
+        }
+    }
+
+    public void limpar() {
+        historico.clear();
+    }
+
+}
