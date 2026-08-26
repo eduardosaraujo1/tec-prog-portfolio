db7aeef7f83361d129abfc384cf420e1d5ce5a01
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-08-25 21:43:52 -0300

    feat: console clear util

--- ApiPokemon/src/main/java/br/edu/fatecpg/pokemon/ui/FavoritesView.java
+++ ApiPokemon/src/main/java/br/edu/fatecpg/pokemon/ui/FavoritesView.java
@@ -1,22 +1,25 @@
 package br.edu.fatecpg.pokemon.ui;
 
+import br.edu.fatecpg.pokemon.data.PokemonRepository;
 import java.util.Scanner;
 
 public class FavoritesView implements View {
 
-    // UI handling the favorites: paginates each view with detailed pokemon information
-    // Has option to "remove" with confirmation dialog
-    // Has option to "add" a pokemon by name (internally call the API to store details in the database)
-    //
     private final Scanner scanner;
+    private final PokemonRepository repository;
 
-    public FavoritesView(Scanner scanner) {
+    public FavoritesView(Scanner scanner, PokemonRepository repository) {
         this.scanner = scanner;
+        this.repository = repository;
     }
 
     @Override
     public void loop() {
-        // TODO Auto-generated method stub
+        // Pegar lista de pokemons favoritados e guardar no estado interno
+        // Exibir um pokemon por vez, permitindo navegar para esquerda e direita
+        // Mostrar página atual e a quantidade de páginas (literalmente array.length())
+        // Para o pokemon atual, permitir operações de remover (adicionar diálogo de confirmação))
+        // Por fim, adicionar opção de adicionar um novo pokemon a partir do nome (literalmente chamada do método correspondente em PokemonRepository)
         throw new UnsupportedOperationException("Unimplemented method 'loop'");
     }
 }
--- ApiPokemon/src/main/java/br/edu/fatecpg/pokemon/ui/PokemonListView.java
+++ ApiPokemon/src/main/java/br/edu/fatecpg/pokemon/ui/PokemonListView.java
@@ -1,18 +1,24 @@
 package br.edu.fatecpg.pokemon.ui;
 
+import br.edu.fatecpg.pokemon.data.PokemonRepository;
 import java.util.Scanner;
 
 public class PokemonListView implements View {
 
     private final Scanner scanner;
+    private final PokemonRepository repository;
+    private final int POKEMON_COUNT = 15;
 
-    public PokemonListView(Scanner scanner) {
+    public PokemonListView(Scanner scanner, PokemonRepository repository) {
         this.scanner = scanner;
+        this.repository = repository;
     }
 
     @Override
     public void loop() {
-        // TODO Auto-generated method stub
+        // Exibir uma lista de 15 pokemons
+        // Dar opções para navegar (próximo ou anterior)
+        // Dar opção para voltar ao menu anterior.
         throw new UnsupportedOperationException("Unimplemented method 'loop'");
     }
 }
